a9530d4f1fb441aae61c546f1a51ee93ec486662
Brian J. Cardiff <bcardiff@manas.com.ar>
2016-11-30 12:59:02 -0300

    Helpers clean up

--- src/Helpers.elm
+++ src/Helpers.elm
@@ -1,7 +1,6 @@
 module Helpers
     exposing
         ( deferredCmd
-        , deferredTask
         )
 
 import Time exposing (Time)
@@ -14,11 +13,3 @@ deferredCmd delay a =
     Task.perform (always a) (Process.sleep delay)
 
 
-deferredTask : Time -> a -> Task Never a
-deferredTask delay a =
-    sleepSafe delay |> Task.andThen (always <| Task.succeed a)
-
-
-sleepSafe : Time -> Task Never ()
-sleepSafe =
-    Process.sleep >> (Task.mapError never)
